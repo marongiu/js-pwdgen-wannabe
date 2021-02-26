d13e875571709f893f61f9988e527be7f3835d19
Andrea <andreamarongiu05@gmail.com>
2021-02-26 15:17:06 +0100

    Fine esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 // Chiedi nome utente
 
 var chiediNome = prompt("Come ti chiami?")
+console.log(chiediNome);
+
+// Chiedi cognome
+
+var chiediCognome = prompt("Qual'è il tuo cognome?")
+console.log(chiediCognome);
+
+// Chiedo il colore preferito
+
+var chiediColore = prompt("Qual'è il tuo colore preferito?")
+console.log(chiediColore);
+
+// Unisco i 3 elementi per formare la password
+
+document.getElementById('password').innerHTML = chiediNome + chiediCognome + chiediColore + "21";
